8b57e31ddcffe910b0a5e2052ee29d7646994c1a
Yulia <yuliafamusova@gmail.com>
2024-04-03 19:29:31 +0300

    добавлен адаптив модалок авторизации

--- js/max.js
+++ js/max.js
@@ -3,6 +3,9 @@ const modalEnter = document.querySelector("#modalAuthReg"); // Модалка в
 const modalReg = document.querySelector("#modalReg"); //Модалка регистрации
 const modalAuth = document.querySelector("#modalAuth"); // Модалка входа
 const modalForgotPassword = document.querySelector("#modalForgotPassword");
+const modalForgotPassword2 = document.querySelector("#modalForgotPassword2");
+const modalForgotPassword3 = document.querySelector("#modalForgotPassword3");
+const modalConfirmReg = document.querySelector("#modalConfirmReg");
 
 // Кнопки
 const closeModal = document.querySelectorAll(".close-modal"); // Кнопка закрытия
@@ -15,6 +18,9 @@ const openModalAuthRegMobile = document.querySelector(
   "#openModalAuthRegMobile"
 ); //Кнопка "вход" на мобилке
 const forgotPassword = document.querySelector("#forgotPassword"); //Кнопка забыли пароль?
+const forgotPassword2 = document.querySelector("#forgotPassword2");
+const forgotPassword3 = document.querySelector("#forgotPassword3");
+const openModalConfirmReg = document.querySelector("#openModalConfirmReg");
 
 //Хедер блюр
 const headerBlur = document.querySelector("header");
@@ -68,6 +74,15 @@ openModalAuthRegMobile.addEventListener("click", () => {
 forgotPassword.addEventListener("click", () => {
   openModalFunc(modalForgotPassword);
 });
+// forgotPassword2.addEventListener("click", () => {
+//   openModalFunc(modalForgotPassword2);
+// });
+forgotPassword3.addEventListener("click", () => {
+  openModalFunc(modalForgotPassword3);
+});
+// openModalConfirmReg.addEventListener("click", () => {
+//   openModalFunc(modalConfirmReg);
+// });
 
 //ПО КЛИКУ НА ГЛАЗИК ПОКАЗЫВАЕТ И СКРЫВАЕТ ПАРОЛЬ
 
@@ -103,3 +118,189 @@ $("body").on("click", ".password-control3", function () {
   }
   return false;
 });
+
+//Ограничиваем ввод количества чисел в модалке забыли пароль 2
+
+const inputNumberforgotPass2 = document.querySelector(
+  "#inputNumberforgotPass2"
+);
+
+document
+  .getElementById("inputNumberforgotPass2")
+  .addEventListener("input", function () {
+    // Получаем значение из поля ввода
+    let inputValue = this.value;
+    // Ограничиваем количество символов
+    let maxLength = 4; // Например, ограничить ввод тремя символами
+
+    if (inputValue.length > maxLength) {
+      // Если количество символов превышает допустимое, обрезаем введенное значение
+      this.value = inputValue.slice(0, maxLength);
+    }
+  });
+
+// Обратный отсчет в модалке забыли пароль 2
+forgotPassword2.addEventListener("click", function () {
+  openModalFunc(modalForgotPassword2);
+
+  const deadline = new Date(new Date().getTime() + 60 * 1000);
+  let timerId = null;
+
+  function countdownTimer() {
+    const diff = deadline - new Date();
+    if (diff <= 0) {
+      clearInterval(timerId);
+    }
+
+    // Calculate minutes and seconds
+    const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) : 0;
+    const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+
+    // Display minutes and seconds
+    $minutes.textContent = minutes < 10 ? "0" + minutes : minutes;
+    $seconds.textContent = seconds < 10 ? "0" + seconds : seconds;
+  }
+
+  // Select HTML elements for minutes and seconds
+  const $minutes = document.querySelector(".timer__minutes");
+  const $seconds = document.querySelector(".timer__seconds");
+
+  // Initialize timer
+  countdownTimer();
+
+  // Start the countdown timer
+  timerId = setInterval(countdownTimer, 1000);
+});
+
+// Валидация;
+
+let form = document.getElementById("form-validate");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let validation = new JustValidate("#form-validate");
+  let isValid = validation.validate();
+
+  validation
+    .addField("#surname", [
+      {
+        rule: "required",
+        errorMessage: "Введите фамилию",
+      },
+      {
+        rule: "customRegexp",
+        value: /[a-zа-яё]/i,
+        errorMessage: "Введите корректную фамилию",
+      },
+      {
+        rule: "maxLength",
+        value: 100,
+        errorMessage: "Фамилия не может содержать больше 100 символов",
+      },
+    ])
+
+    .addField("#name", [
+      {
+        rule: "required",
+        errorMessage: "Введите имя",
+      },
+      {
+        rule: "customRegexp",
+        value: /[a-zа-яё]/i,
+        errorMessage: "Введите корректное имя",
+      },
+      {
+        rule: "maxLength",
+        value: 100,
+        errorMessage: "Имя не может содержать больше 100 символов",
+      },
+    ])
+
+    .addField("#email", [
+      {
+        rule: "required",
+        errorMessage: "Введите E-mail",
+      },
+      {
+        rule: "email",
+        errorMessage: "Введите корректный E-mail",
+      },
+      {
+        rule: "maxLength",
+        value: 256,
+        errorMessage: "E-mail не может содержать больше 256 символов",
+      },
+    ])
+
+    .addField("#password-input", [
+      {
+        rule: "required",
+        errorMessage: "Введите пароль",
+      },
+      {
+        rule: "maxLength",
+        value: 64,
+        errorMessage: "Пароль не может содержать больше 64 символов",
+      },
+    ]);
+
+  if (isValid) {
+    openModalConfirmReg.addEventListener("click", () => {
+      openModalFunc(modalConfirmReg);
+    });
+  }
+});
+
+// let validation = new JustValidate("#form-validate", {
+//   rules: {
+//     surname: {
+//       required: true,
+//       maxLength: 100,
+//       custom: /[a-zа-яё]/i,
+//     },
+//     name: {
+//       required: true,
+//       maxLength: 100,
+//       custom: /[a-zа-яё]/i,
+//     },
+//     email: {
+//       required: true,
+//       email: true,
+//       maxLength: 256,
+//     },
+//     "password-input": {
+//       required: true,
+//       maxLength: 64,
+//     },
+//   },
+//   messages: {
+//     surname: {
+//       required: "Введите фамилию",
+//       maxLength: "Фамилия не может содержать больше 100 символов",
+//       custom: "Введите корректную фамилию",
+//     },
+//     name: {
+//       required: "Введите имя",
+//       maxLength: "Имя не может содержать больше 100 символов",
+//       custom: "Введите корректное имя",
+//     },
+//     email: {
+//       required: "Введите E-mail",
+//       email: "Введите корректный E-mail",
+//       maxLength: "E-mail не может содержать больше 256 символов",
+//     },
+//     "password-input": {
+//       required: "Введите пароль",
+//       maxLength: "Пароль не может содержать больше 64 символов",
+//     },
+//   },
+// });
+
+// let form = document.getElementById("form-validate");
+
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   if (validation.validate()) {
+//     openModalFunc(modalConfirmReg);
+//   }
+// });
