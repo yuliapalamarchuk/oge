7adff95ff42daba598d75e9d3a1ade95941f593d
maxim1955 <nikitin.maxim.99@gmail.com>
2024-05-24 20:53:31 +0300

    before pull

---
+++ js/send-form.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    console.log('Start')
+    //  Процесс регистрации
+    let surname = document.querySelector('#surname')
+    let name = document.querySelector('')
+
+})
